5f0ed83747795acf852760a6681ece7e5d2c3882
Florian Crafter <55284712+FlorianStrobl@users.noreply.github.com>
2021-03-13 20:31:06 +0100

    Create SlashCommand-Roles.ts

---
+++ Scripts/Commands/SlashCommand-Roles.ts
@@ -0,0 +1,47 @@
+// Give user roles they want with custom name and with the role Ids
+// edit all lines with the comment EDIT to make the code work
+
+// EDIT Role names (has to be same as in Role)
+const choices: string[] = ['role 1', 'role 2', 'role 3'];
+
+// EDIT Role name and its ID
+const Role: [string, string][] = [
+  ['role 1', 'id'],
+  ['role 2', 'id'],
+  ['role 3', 'id']
+];
+
+// EDIT channels in which the command can be executed
+const channels = ['channel id 1', 'channel id 2'];
+
+discord.interactions.commands.register(
+  {
+    name: 'role',
+    description: 'Give yourself a role.',
+    showSourceMessage: true,
+    options: (args) => ({
+      role: args.string({
+        name: 'role',
+        description: 'The role you want.',
+        required: true,
+        choices: choices
+      })
+    })
+  },
+  async (msg, { role }) => {
+    if (!channels.includes(msg.channelId) && channels.length !== 0)
+      await msg.acknowledge();
+
+    const index = Role.findIndex((t) => t[0] == role);
+    if (index !== -1) {
+      try {
+        await msg.member.addRole(Role[index][1]);
+        await msg.respond(`You have now the <@&${Role[index][1]}> role!`);
+      } catch (_) {
+        await msg.respond(`Error with the ${role} role.`);
+      }
+    } else await msg.respond(`Error with the ${role} role.`);
+
+    await msg.acknowledge();
+  }
+);
